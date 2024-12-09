16e1375f72a76e271ab0dacb3dcdcf24bb3f1233
PatienceCrixix1z12 <nutsugahamapatience12@gmail.com>
2024-12-09 12:08:19 +0000

    add all changes and files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let dom =document.querySelector(".dom")
+dom.style.backgroundColor ="blue"
+dom.style.color ="black"
+dom.innerHTML ="I know now what the DOM is 🙌👏💅"
+
+let access =document.querySelector(".access")
+access.style.backgroundColor ="yellow"
+access.style.color ="blue"
+access.innerHTML ="I now know access/select elements in Dom💅 purr"
+
+let use =document.querySelector(".use")
+use.style.backgroundColor= "purple"
+use.style.color ="orange"
+use.innerHTML ="I know how to style methods to change the style of an element in Dom"
+
+let style =document.querySelector(".style")
+style.style.backgroundColor ="grey"
+style.style.color ="pink"
+style.innerHTML ="I know how to styling properties differ in JS from CSS🥰😘"
+
+let excited =document.querySelector(".excited")
+excited.style.background ="cyan"
+excited.style.color ="orange"
+excited.innerHTML ="Yes I did it !! I manipulated the DOM and styled my element...hurray🎉🎉💕"
